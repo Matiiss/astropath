bbedfc5847bdcf6a779ceaee516f896b84b19e77
Matiiss <83066658+Matiiss@users.noreply.github.com>
2023-11-30 20:48:40 +0200

    cleaned up setup.py

--- setup.py
+++ setup.py
@@ -4,9 +4,7 @@ setup(
     ext_modules=[
         Extension(
             name="cstar.astar",
-            sources=["src/astar.c", "src/AS_heap.c", "src/AS_stack.c", "src/AS_astar.c"],
-            # include_dirs=["src"],
-            # extra_objects=["objects/AS_heap.o", "objects/AS_stack.o", "objects/AS_astar.o"],
+            sources=["src/astar.c", "src/AS_heap.c", "src/AS_stack.c", "src/AS_astar.c"]
         ),
     ]
 )
