b1d5b716b405f1b0a687fe64608eb5a9c803c6c6
Matiiss <83066658+Matiiss@users.noreply.github.com>
2023-12-02 22:47:42 +0200

    project name changed to astropath

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import Extension, setup
 setup(
     ext_modules=[
         Extension(
-            name="cstar.astar",
+            name="astropath.astar",
             sources=["src/astar.c", "src/AS_heap.c", "src/AS_stack.c", "src/AS_astar.c", "src/AS_list.c", "src/AS_dict.c"]
         ),
     ]
